9284694f5d20032668c52c00762a17fa1736400c
Saeed Motedaveli <saeed.motedaveli@gmail.com>
2015-11-07 22:03:26 +0330

    Create final_check.m

---
+++ final_check.m
@@ -0,0 +1,44 @@
+% final_check.m
+% @author Saeed Motedaveli
+
+if power_sum < Pr
+    arg = 0;
+    for i = 1 : num
+        if P(i) == eval(['p', int2str(i), '(1)'])
+            arg = i;
+            break;
+        end
+    end
+    
+    delta_power = (Pr - power_sum) + eval(['p', int2str(arg), '(1)']);
+    for i = 1 : eval(['p', int2str(arg), '(length(p', int2str(arg), '));'])
+        if delta_power < eval(['p', int2str(arg), '(', int2str(i), ')'])
+            break;
+        end
+    end
+    
+    P(arg) = delta_power;
+    lambda_final(arg) =  polyval(polyfit([eval(['p', int2str(arg), '(', int2str(i - 1), ')']) eval(['p', int2str(arg), '(', int2str(i), ')'])], [eval(['lambda', int2str(arg), '(', int2str(i - 1), ')']), eval(['lambda', int2str(arg), '(', int2str(i), ')'])], 1), delta_power);
+    
+end
+
+if power_sum > Pr
+    arg = 0;
+    for i = 1 : num
+        if P(i) == eval(['p', int2str(i), '(length(p', int2str(i), '))'])
+            arg = i;
+            break;
+        end
+    end
+    
+    delta_power = Pr - (power_sum - eval(['p', int2str(arg), '(length(p', int2str(arg),'))']));
+    for i = 1 : eval(['p', int2str(arg), '(length(p', int2str(arg), '));'])
+        if delta_power < eval(['p', int2str(arg), '(', int2str(i), ')'])
+            break;
+        end
+    end
+    
+    P(arg) = delta_power;
+    lambda_final(arg) =  polyval(polyfit([eval(['p', int2str(arg), '(', int2str(i - 1), ')']) eval(['p', int2str(arg), '(', int2str(i), ')'])], [eval(['lambda', int2str(arg), '(', int2str(i - 1), ')']), eval(['lambda', int2str(arg), '(', int2str(i), ')'])], 1), delta_power);
+    
+end
